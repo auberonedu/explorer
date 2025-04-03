3e604794cdbe2d57ef463bf7be0635e5d4e83a99
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-04-03 16:48:56 -0700

    Added more JUnit tests for reachableArea method, testing the edge cases

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -15,6 +15,28 @@ public class ExplorerSearchTest {
         assertEquals(14, actual);
     }
 
+    @Test
+    public void testReachableArea_allAccessible() {
+        int[][] island = {
+            {1, 0, 1},
+            {1, 1, 1},
+            {1, 1, 1},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(9, actual);  
+    }
+
+    @Test
+    public void testReachableArea_noAccessible() {
+        int[][] island = {
+            {2, 2, 2},
+            {2, 0, 2},
+            {2, 2, 2},
+        };
+        int actual = ExplorerSearch.reachableArea(island);
+        assertEquals(1, actual);  
+    }
+
     // Add more tests here!
     // Come up with varied cases
 }
