d8f8049381e8e1982d9926208b00e3d8f37632fb
Auberon López <auberonedu@gmail.com>
2025-04-03 12:08:51 -0700

    cheesier

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -15,6 +15,58 @@ public class ExplorerSearchTest {
         // assertEquals(14, actual);
     }
 
+    @Test
+    public void testReachableArea_someUnreachabl1e() {
+        // int[][] island = {
+        //     {1,1,1,3,1,1},
+        //     {3,2,3,1,3,1},
+        //     {1,1,1,1,3,3},
+        //     {3,1,2,1,0,1},
+        //     {1,1,1,2,1,1},
+        // };
+        // int actual = ExplorerSearch.reachableArea(island);
+        // assertEquals(14, actual);
+    }
+
+    @Test
+    public void testReachableArea_someUnreachable1() {
+        // int[][] island = {
+        //     {1,1,1,3,1,1},
+        //     {3,2,3,1,3,1},
+        //     {1,1,1,1,3,3},
+        //     {3,1,2,1,0,1},
+        //     {1,1,1,2,1,1},
+        // };
+        // int actual = ExplorerSearch.reachableArea(island);
+        // assertEquals(14, actual);
+    }
+
+    @Test
+    public void testReachableArea_someUnreach3able2() {
+        // int[][] island = {
+        //     {1,1,1,3,1,1},
+        //     {3,2,3,1,3,1},
+        //     {1,1,1,1,3,3},
+        //     {3,1,2,1,0,1},
+        //     {1,1,1,2,1,1},
+        // };
+        // int actual = ExplorerSearch.reachableArea(island);
+        // assertEquals(14, actual);
+    }
+
+    @Test
+    public void testReachableArea_someUnr3eachable() {
+        // int[][] island = {
+        //     {1,1,1,3,1,1},
+        //     {3,2,3,1,3,1},
+        //     {1,1,1,1,3,3},
+        //     {3,1,2,1,0,1},
+        //     {1,1,1,2,1,1},
+        // };
+        // int actual = ExplorerSearch.reachableArea(island);
+        // assertEquals(14, actual);
+    }
+
     // Add more tests here!
     // Come up with varied cases
 }
