4c49aa30f498449ffb825d9630c8b699e90db30a
Madeleine Chance <mechance782@gmail.com>
2025-04-04 12:12:53 -0700

    Add tests for nearbyFields

--- src/ExplorerSearchTest.java
+++ src/ExplorerSearchTest.java
@@ -1,4 +1,8 @@
 import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class ExplorerSearchTest {
@@ -90,4 +94,101 @@ public class ExplorerSearchTest {
 
 
     // !! nearbyFields tests
+
+    @Test
+    public void testNearbyFields_currentOnStart(){
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,0,3,3},
+            {3,1,2,1,2,1},
+            {1,1,1,2,1,1},
+        };
+        List<int[]> actual = ExplorerSearch.nearbyFields(island, new int[]{2, 3});
+        List<int[]> expected = new ArrayList<>();
+        expected.add(new int[]{1, 3});
+        expected.add(new int[]{3, 3});
+        expected.add(new int[]{2, 2});
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNearbyFields_currentInCorner(){
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,0,3,3},
+            {3,1,2,1,2,1},
+            {1,1,1,2,1,1},
+        };
+        List<int[]> actual = ExplorerSearch.nearbyFields(island, new int[]{0, 0});
+        List<int[]> expected = new ArrayList<>();
+        expected.add(new int[]{0, 1});
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNearbyFields_currentOnRightCoast(){
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,0,3,3},
+            {3,1,2,1,2,1},
+            {1,1,1,2,1,1},
+        };
+        List<int[]> actual = ExplorerSearch.nearbyFields(island, new int[]{3, 5});
+        List<int[]> expected = new ArrayList<>();
+        expected.add(new int[]{4, 5});
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNearbyFields_currentOnBottomCoast(){
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,0,3,3},
+            {3,1,2,1,2,1},
+            {1,1,1,2,1,1},
+        };
+        List<int[]> actual = ExplorerSearch.nearbyFields(island, new int[]{4, 1});
+        List<int[]> expected = new ArrayList<>();
+        expected.add(new int[]{4, 0});
+        expected.add(new int[]{4, 2});
+        expected.add(new int[]{3, 1});
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNearbyFields_noFields(){
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,2,3,3},
+            {3,1,2,0,2,1},
+            {1,1,1,2,1,1},
+        };
+        List<int[]> actual = ExplorerSearch.nearbyFields(island, new int[]{3, 3});
+        List<int[]> expected = new ArrayList<>();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testNearbyFields_allFields(){
+        int[][] island = {
+            {1,1,1,3,1,1},
+            {3,2,3,1,3,1},
+            {1,1,1,1,3,3},
+            {3,1,1,0,1,1},
+            {1,1,1,1,1,1},
+        };
+        List<int[]> actual = ExplorerSearch.nearbyFields(island, new int[]{3, 3});
+        List<int[]> expected = new ArrayList<>();
+        expected.add(new int[]{4, 3});
+        expected.add(new int[]{2, 3});
+        expected.add(new int[]{3, 4});
+        expected.add(new int[]{3, 2});
+        assertEquals(expected, actual);
+    }
+
 }
